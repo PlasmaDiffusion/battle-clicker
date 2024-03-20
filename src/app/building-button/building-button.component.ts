3d8b545493a35e5249718bbe0262bf39eb2daebf
Scott Cooper <18429915+PlasmaDiffusion@users.noreply.github.com>
2024-03-20 15:49:00 -0400

    Chest grows with scss hover

--- src/app/building-button/building-button.component.ts
+++ src/app/building-button/building-button.component.ts
@@ -17,13 +17,10 @@ export class BuildingButtonComponent {
   constructor() {
     this.building = { name: '', price: 0, owned: 0, goldPerSecond: 0 };
     this.gold = 0;
-    console.log("sdfds");
   }
 
   onClick() {
-    console.log("clicked", this.building.name);
-    console.log(this.gold, ">", this.building.price);
-    if (this.gold > this.building.price) {
+    if (this.gold >= this.building.price) {
       this.onBuy.emit(this.building);
     }
   }
